dd315f3f054953116999bc5577fd4290cf4914af
cinoxil <54507093+cinoxil@users.noreply.github.com>
2022-04-24 21:19:36 +0300

    first release

---
+++ app/src/main/java/com/example/multiuimobileproject/kiloBoyInput.java
@@ -0,0 +1,14 @@
+package com.example.multiuimobileproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kiloBoyInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input_kiloboy);
+    }
+}
