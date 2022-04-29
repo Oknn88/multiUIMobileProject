b1d8a92885dec186abe213c2cc03e0a5d923b318
cinoxil <54507093+cinoxil@users.noreply.github.com>
2022-04-29 15:25:35 +0300

    DataBase added

---
+++ app/src/main/java/com/example/multiuimobileproject/Entities/kiloBoy.java
@@ -0,0 +1,13 @@
+package com.example.multiuimobileproject.Entities;
+
+import android.text.Editable;
+
+import java.util.Date;
+
+public class kiloBoy {
+
+    public String kilo;
+    public String boy;
+    public String tarih;
+
+}
