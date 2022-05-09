64744d64dfd307197cc2d76bcf11358507369284
cinoxil <54507093+cinoxil@users.noreply.github.com>
2022-05-09 13:34:49 +0300

    Revert "Sync"
    This reverts commit ea02ac7ccdeef2f20b0bd70ecccfa5aedebd227e.

--- app/src/main/java/com/example/multiuimobileproject/NotAdapter.java
+++ app/src/main/java/com/example/multiuimobileproject/NotAdapter.java
@@ -1,11 +1,20 @@
 package com.example.multiuimobileproject;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.Person;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.SyncStateContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.multiuimobileproject.databinding.RecyclerRowBinding;
@@ -17,6 +26,8 @@ public class NotAdapter extends RecyclerView.Adapter<NotAdapter.NotHolder> //alt
 {
 
     ArrayList<Nots> notsArrayList;
+
+
 
     public NotAdapter(ArrayList<Nots> notsArrayList)
     {
@@ -33,17 +44,48 @@ public class NotAdapter extends RecyclerView.Adapter<NotAdapter.NotHolder> //alt
     @Override
     public void onBindViewHolder(@NonNull NotHolder holder, int position) {
 
-        holder.binding.rvNotumid.setText(notsArrayList.get(position).notum);
-        holder.binding.rvtarihid.setText(notsArrayList.get(position).tarihim);
-        holder.binding.rvsaatid.setText(notsArrayList.get(position).saatim);
+        Context context = holder.itemView.getContext();
+        holder.binding.rvNotumid.setText(notsArrayList.get(holder.getAdapterPosition()).notum);
+        holder.binding.rvtarihid.setText(notsArrayList.get(holder.getAdapterPosition()).tarihim);
+        holder.binding.rvsaatid.setText(notsArrayList.get(holder.getAdapterPosition()).saatim);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(holder.itemView.getContext(),NotalActivity.class);
                 intent.putExtra("info","old");
-                intent.putExtra("notid",notsArrayList.get(position).id);
+                intent.putExtra("notid",notsArrayList.get(holder.getAdapterPosition()).id);
                 holder.itemView.getContext().startActivity(intent);
+            }
+        });
+
+
+        holder.binding.kisiyisilmeImageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                new AlertDialog.Builder(context)
+                        .setTitle("Notu Sil")
+                        .setMessage("Notu Listeden Kaldırmak İstermisiniz?")
+                        .setNegativeButton("Hayır", null)
+                        .setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // SQLiteDatabase
+
+                                //listeden silme
+
+                                notsArrayList.remove(holder.getAdapterPosition());
+                                notifyItemRemoved(holder.getAdapterPosition());
+
+
+                            }
+                        }).show();
+
+
+
+              //  Cursor cursor=database.rawQuery("DELETE FROM notlar WHERE id=?",new String[]);
             }
         });
     }
